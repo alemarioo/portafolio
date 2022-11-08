bee61f3fea916f7e3013a13b1aa6443d1a7a2966
mario ruano <marioruano25@gmail.com>
2022-11-07 20:51:14 -0600

    deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MeComponent } from './pages/me/me.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
